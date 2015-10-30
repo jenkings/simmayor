f2f28ecf2a426bc1ba9a0b18fd488bd97c04630c
MartinZatkovic <zatkovic.martin@gmail.com>
2015-10-30 20:42:46 +0100

    Vylsedna v0.1 - DB v puvodním stavu

--- files/cfg/host.php
+++ files/cfg/host.php
@@ -1,9 +1,9 @@
 <?php
 define("DB_HOST",     "localhost");
 define("DB_NAME",     "simmayor");
-define("DB_PASS",     "");
-define("DB_USER",     "simmayor");
+define("DB_PASS",     "root");
+define("DB_USER",     "root");
 
-define("WEB_ROOT",     "http://sim-mayor.localhost");
+define("WEB_ROOT",     "http://localhost/simmayor");
 include_once "game-limits.php";
 ?>

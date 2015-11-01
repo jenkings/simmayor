946e54ef35689c5468e0e24731f7fb3c87148542
MartinZatkovic <zatkovic.martin@gmail.com>
2015-11-01 11:48:28 +0100

    Založení větve - Vzhled + Označení administrace

--- files/admin/index.php
+++ files/admin/index.php
@@ -3,7 +3,7 @@
 <head>	
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="style.css">
-<title>Sim Mayor administrace</title>
+<title>Sim Mayor administrace  - NEW</title>
 </head>
 <body>
 	

995993fb694c3e6a201c91f6fbbf4b9ff37abc25
MartinZatkovic <zatkovic.martin@gmail.com>
2015-11-01 13:11:48 +0100

    Funkčnost hry v sekci Administrace fungovala

--- files/cfg/host.php
+++ files/cfg/host.php
@@ -5,6 +5,7 @@ define("DB_PASS",     "");
 define("DB_USER",     "simmayor");
 
 define("WEB_ROOT",     "http://sim-mayor.localhost");
+define("WEB_ROOT_GAME",     "http://sim-mayor.localhost/game.php");
 
 include_once "game-limits.php";
 ?>

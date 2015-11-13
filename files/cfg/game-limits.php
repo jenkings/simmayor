d09b1d16cda08152ee5e3490b2b07a65570edccb
„adoksnaj“ <adoksnaj@gmail.com>
2015-11-13 09:39:25 +0100

    přidány configy pro maximum položek v shopu

--- files/cfg/game-limits.php
+++ files/cfg/game-limits.php
@@ -7,4 +7,8 @@ define("ISLAND_BASE_VALUE",  "100");
 define("SUM_ISLAND_PAYBACK","300000");
 
 define("STARTING_MONEY","50000");
+
+define("PLAYER_SHOP_ITEMS_LIMIT","4");
+define("VIP_SHOP_ITEMS_LIMIT","15");
+
 ?>

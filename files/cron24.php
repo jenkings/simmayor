e4a34538e012b076e7f45e62ec5d84d69184adf1
„adoksnaj“ <adoksnaj@gmail.com>
2015-11-15 20:55:17 +0100

    odstraněna loterie

--- files/cron24.php
+++
@@ -1,9 +0,0 @@
-<?php
-function __autoload($class_name) {include './classes/'.$class_name . '.class.php';}	
-$db = new Database('localhost','root','unsupportedpassword','simmayor');
-			
-$loterie = new Loterie($db);
-$loterie->novytah();
-$loterie->vyhodnotLosy();
-$loterie->DeleteTickets();
-?>

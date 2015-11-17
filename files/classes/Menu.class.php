4ba0aab2ea18ee06cf2c695ca5ad791d3622995c
Jenkings <adoksnaj@seznam.cz>
2015-11-17 15:58:45 +0100

    Vytvořen template System

---
+++ files/classes/Menu.class.php
@@ -0,0 +1,25 @@
+<?php
+class Menu{
+	
+	public function __toString(){
+		$out = "";
+		if (!isset($_SESSION)) session_start();
+			$out.="<ul id='menu'>";
+			$out.="<li><a href='index.php'>Domů</a></li>";
+			$out.="<li><a href='game.php'>Hra</a></li>";
+			$out.="<li><a href='./forum/index.php'>Fórum</a></li>";
+			$out.="<li><a href='top.php'>Top</a></li>";
+			
+			if(isset($_SESSION['prihlasen'])){
+				$out.="<li><a href='obchod.php'>Obchod</a></li>";
+				$out.="<li><a href='banka.php'>Banka</a></li>";
+				$out.="<li><a href='islandset.php'>Poplatky</a></li>";
+				$out.="<li><a href='showme.php'>Profil</a></li>";
+				$out.="<li><a href='logout.php'>Odhlásit</a></li>";
+			}
+			$out.="</ul>";
+		return $out;
+	}
+	
+}
+?>
--- files/index.php
+++ files/index.php
@@ -1,64 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>	
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="style.css">
-<title>SimMayor</title>
-</head>
-
-<body>
+<?php
+	include_once "./cfg/host.php";
+	include_once "./classes/Database.class.php";
+	include_once "./classes/Reklama.class.php";
+	include_once "./classes/layout/Template.class.php";
+	include_once "./classes/Menu.class.php";
+			
+	$tpl = new Template();
+	$db = new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 	
-<header>
-	<img src="logo.png" alt="logo">
-	<?php include "menu.php";?>
-</header>	
-
-<div id="page">
-
-	<div id="content">
-			
-			<div id="leftpanel">
-				<p>Veškeré novinky a informace o updatech naleznete v našem <a href="./forum/showthread.php?topicid=16">fóru</a></p>
-				<hr>
-				<p>Založte si účet a hned začněte hrát</p><button><a href="./registrace.php">Registrovat</a></button>
-				<hr>
-				<img src="http://img19.imageshack.us/img19/1060/48mz.jpg" alt="centrum">
-				<img src="http://imageshack.us/a/img811/752/3qcz.jpg" alt="Náhled">
-				<img src="http://img827.imageshack.us/img827/8894/2e13.jpg" alt="chatky">
-				<img src="http://imageshack.us/a/img96/1413/mobx.jpg" alt="Přístav">
-			</div>
-			
-			
-			<div class="tip">
-				<img src="./ind3.png" alt="náhled" width="220">
-				<p>Můžete vyzkoušet své štěstí v loterii, a pokusit se tak vylepšit svůj rozpočet</p>
-			</div>
-			
-			<div class="tip">
-				<img src="./ind1.png" alt="náhled" width="220">
-				<p>Poradce vám napoví co děláte špatně, a jak můžete zvýšit zisky svého ostrova</p>
-			</div>
-			
-			<div class="tip">
-				<img src="./ind2.png" alt="náhled" width="380">
-				<p style="width:300px;">Vybudujte si ostrov přesně podle vašich představ a staňte se jeho starostou.Je jen na vás jakou strategii zvolíte, a kolik peněz si vyděláte.</p>
-			</div>
-			
-			<?php
-			function __autoload($class_name) {include './classes/'.$class_name . '.class.php';}
+	$reklamy = new Reklama($db);
+	$obsah = new Template("title_page");
+	$obsah->setContent("bannery",$reklamy->Bannery());
+	
+	$menu = new Menu();
+	$tpl->setContent("menu",$menu);
+	
+	
+	$tpl->setContent("content",$obsah);
+	
+	echo $tpl;
 		
-			include_once "./cfg/host.php";
-			$db = new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-			$reklamy = new Reklama($db);
-			echo $reklamy->Bannery();
+?>
 
-			?>
-			
-	<div style="clear: left;"></div>
-</div>
-</div>
-
-<footer>Copyright Jan "Jenkings" Škoda | Code by <a href="http://jenkings.eu">Jenkings</a> | Design by <a href="#">Ex_0</footer>
-<?php include "analytics.php";?>
-</body>
-</html>

ada7d49bd345ba296896f3d766742d4cb8b09d5a
MartinZatkovic <zatkovic.martin@gmail.com>
2015-12-19 15:27:31 +0100

    Úprava na domácí server

--- files/cfg/host.php
+++ files/cfg/host.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST",     "localhost");
 define("DB_NAME",     "simmayor");
-define("DB_PASS",     "root");
-define("DB_USER",     "root");
+define("DB_PASS",     "");
+define("DB_USER",     "simmayor");
 
 define("WEB_ROOT",     "http://localhost/simmayor");
 ?>

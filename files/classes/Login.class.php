5f97039d335b7172d420551e353f15d3da496100
Jenkings <adoksnaj@seznam.cz>
2016-01-25 10:34:58 +0100

    zprovozněno logování

--- files/classes/Login.class.php
+++ files/classes/Login.class.php
@@ -11,6 +11,8 @@ class Login{
 			$row = $this->db->queryOne("SELECT * FROM accounts WHERE jmeno = ? AND heslo = md5(?)",array($nick,$pass));
 			if ($row) {
 				$_SESSION['prihlasen'] = $row['id'];
+				$log = new IPlog($this->db);
+				$log->record($row['id'],$_SERVER['REMOTE_ADDR']);
 				header('Location: index.php?pidgame');
 			}else{
 				header('Location: index.php?pid=register');

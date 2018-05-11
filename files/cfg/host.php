717816961b551fc5e56ef73576e78115823d65c2
Jenkings <adoksnaj@gmail.com>
2018-05-11 08:02:03 +0000

    Update host.php

--- files/cfg/host.php
+++ files/cfg/host.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST",     "localhost");
-define("DB_NAME",     "simmayor");
-define("DB_PASS",     "root");
-define("DB_USER",     "root");
+define("DB_NAME",     "databasename");
+define("DB_PASS",     "password");
+define("DB_USER",     "user");
 
 define("WEB_ROOT",     "http://localhost/simmayor");
 ?>
